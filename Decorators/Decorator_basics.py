72b26b16dc5b244ee9bd925449aee1f8e9c3b529
Sathish Vinayk <sathishvinayk@gmail.com>
2017-03-09 20:16:05 +0530

    Update Decorator_basics.py

--- Decorators/Decorator_basics.py
+++ Decorators/Decorator_basics.py
@@ -3,14 +3,14 @@ class General:
 	updated=0
 	def __init__(self):
 		General.updated=General.updated+1
-	
+
 	#Now define a decorator below
 	@staticmethod
 	def printupdated():
-		print("Total number of %s class instance created" %updated)
+		print("Total number of %s class instance created" %General.updated)
 
 a=General()
 b=General()
 c=General()
-General.updated()
+General.printupdated()
 a.printupdated()
